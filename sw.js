55a0c8b84c6385f8f0fc16515f7636255c929141
dezotg <dezmondreese090@gmail.com>
2026-09-24 18:59:04 -0400

    Delete sw.js

--- sw.js
+++
@@ -1,8 +0,0 @@
-const CACHE='dez-fuel-barcode-v3';
-const ASSETS=['./','./index.html','./manifest.webmanifest'];
-self.addEventListener('install',e=>{self.skipWaiting();e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)))});
-self.addEventListener('activate',e=>e.waitUntil(Promise.all([self.clients.claim(),caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k))))])));
-self.addEventListener('fetch',e=>{
-  if(e.request.url.includes('openfoodfacts.org') || e.request.url.includes('unpkg.com')) return;
-  e.respondWith(fetch(e.request).catch(()=>caches.match(e.request)));
-});
